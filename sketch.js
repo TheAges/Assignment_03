5ae8afbc63376c71b6f2c837380eee87f6243678
TheAges <dragoneage@gmail.com>
2017-10-15 19:55:19 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,20 +7,9 @@
        RAND2='#3c5979';
        click=false;
        first=true;
+       keys=false;
+       quadr=true;
        T=0;
-       
-       
-var colorList1 = ['#e86584',
-                 '#3c5979',
-                 '#018cb7',
-                 '#fae093'];
-                 
-var colorList2 = ['#e86584',
-                 '#3c5979',
-                 '#018cb7',
-                 '#fae093'];
-
-
 
 function setup() {
   createCanvas(500, 500);
@@ -42,21 +31,37 @@ function draw() {
  translate(width/2,(height/2)/*+((2*h/3)-(h/2))*/)  //Centra il Triangolo;
 
  
- rotate(-T)
- scale(S1)
-
+if (keys==false) {rotate(-T); scale(S1)}
+ 
 if (click==true) {
  
  if (T % 50 == 0) {RAND1=[random(255),random(255),random(255)];RAND2=[random(255),random(255),random(255)]}
 
- for (I,S,R;  I<100;  I++/*, R+=5, S=S+1*/ ) {
+ for (I,S,R;  I<120;  I++/*, R+=5, S=S+1*/ ) {
   
   if (I % 2 == 0) {fill(RAND1)} else {fill(RAND2)}
  
-  scale(0.85)
+
+  
+  if (quadr==true)
+  {
+  scale(0.916)
   rotate(5)
   //scale(1-(R/33.3252))
+  beginShape();
+   vertex(0,0);
+   vertex(0,L/2);
+   vertex(L/2,L/2);
+   vertex(L/2,-L/2);
+   vertex(-L/2,-L/2);
+   vertex(-L/2,L/2);
+   vertex(0,L/2);
+  endShape(CLOSE);
+  }
   
+  else {
+  scale(0.85)
+  rotate(5)
   beginShape();
    vertex(0,0);
    vertex(0,h/3);
@@ -65,22 +70,37 @@ if (click==true) {
    vertex(-L/2,h/3);
    vertex(0,h/3);
   endShape(CLOSE);
-  
+  }
  }
 }
 
 else {
 
- for (I,S,R;  I<100;  I++/*, R+=5, S=S+1*/ ) {
+ for (I,S,R;  I<120;  I++/*, R+=5, S=S+1*/ )  {
   
-  
- 
-  scale(0.85)
-  rotate(5)
-  //scale(1-(R/33.3252))
-
   if (I % 2 == 0) {fill("black")} else {fill("white")}
  
+
+  
+  if (quadr==true)
+  {
+  scale(0.916)
+  rotate(5)
+  //scale(1-(R/33.3252))
+  beginShape();
+   vertex(0,0);
+   vertex(0,L/2);
+   vertex(L/2,L/2);
+   vertex(L/2,-L/2);
+   vertex(-L/2,-L/2);
+   vertex(-L/2,L/2);
+   vertex(0,L/2);
+  endShape(CLOSE);
+  }
+  
+  else {
+  scale(0.85)
+  rotate(5)
   beginShape();
    vertex(0,0);
    vertex(0,h/3);
@@ -89,12 +109,16 @@ else {
    vertex(-L/2,h/3);
    vertex(0,h/3);
   endShape(CLOSE);
- 
+  }
  }
 }
 
-if (I==100) {I=0,R=0,S=1}
-if (T==518) {S1=1,T=0}
+if (I==120) {I=0,R=0,S=1}
+
+if (quadr==true && T==212) {S1=1,T=0}
+
+if (quadr==true && T==518) {S1=1,T=0} 
+
 //console.log(T);
 
 
@@ -107,3 +131,9 @@ fill("red")
 }
 
 function mouseClicked() {click=!click; /*first=false*/}
+
+function keyTyped() {
+  if (key === 't') {quadr=false;S1=1,T=0} 
+  else if (key === 'q') {quadr=true;S1=1,T=0}
+  else {keys=!keys;}
+}
