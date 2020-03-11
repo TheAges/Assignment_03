621414fca94536683288ecd90930bffa0de4f2fa
TheAges <dragoneage@gmail.com>
2020-03-11 17:16:13 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@
        T=0;
 
 function setup() {
-  createCanvas(850, 850);
+  createCanvas(700,700);
   angleMode(DEGREES);
   background(235);
 }
@@ -27,22 +27,22 @@ function draw() {
 
  fill("black")
  noStroke()
- 
+
  translate(width/2,(height/2)/*+((2*h/3)-(h/2))*/)  //Centra il Triangolo;
 
- 
+
 if (keys==false) {rotate(-T); scale(S1)}
- 
+
 if (click==true) {
- 
+
  if (T % 50 == 0) {RAND1=[random(255),random(255),random(255)];RAND2=[random(255),random(255),random(255)]}
 
  for (I,S,R;  I<120;  I++/*, R+=5, S=S+1*/ ) {
-  
-  if (I % 2 == 0) {fill(RAND1)} else {fill(RAND2)}
- 
 
-  
+  if (I % 2 == 0) {fill(RAND1)} else {fill(RAND2)}
+
+
+
   if (quadr==true)
   {
   scale(0.916)
@@ -58,7 +58,7 @@ if (click==true) {
    vertex(0,L/2);
   endShape(CLOSE);
   }
-  
+
   else {
   scale(0.85)
   rotate(5)
@@ -77,11 +77,11 @@ if (click==true) {
 else {
 
  for (I,S,R;  I<120;  I++/*, R+=5, S=S+1*/ )  {
-  
-  if (I % 2 == 0) {fill("black")} else {fill("white")}
- 
 
-  
+  if (I % 2 == 0) {fill("black")} else {fill("white")}
+
+
+
   if (quadr==true)
   {
   scale(0.916)
@@ -97,7 +97,7 @@ else {
    vertex(0,L/2);
   endShape(CLOSE);
   }
-  
+
   else {
   scale(0.85)
   rotate(5)
@@ -117,7 +117,7 @@ if (I==120) {I=0,R=0,S=1}
 
 if (quadr==true && T==212) {S1=1,T=0}
 
-if (quadr==true && T==518) {S1=1,T=0} 
+if (quadr==true && T==518) {S1=1,T=0}
 
 //console.log(T);
 
@@ -133,7 +133,7 @@ fill("red")
 function mouseClicked() {click=!click; /*first=false*/}
 
 function keyTyped() {
-  if (key === 't') {quadr=false;S1=1,T=0} 
+  if (key === 't') {quadr=false;S1=1,T=0}
   else if (key === 'q') {quadr=true;S1=1,T=0}
   else {keys=!keys;}
 }
